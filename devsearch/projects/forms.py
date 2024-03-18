35e61d7d5eef08d5f2e95afc4f35fa248272d89f
hammer <ttkagentusolie@gmail.com>
2024-03-18 22:35:29 +0800

    Добавлена форма, добавлены классы к форме. Создал приложение users

--- devsearch/projects/forms.py
+++ devsearch/projects/forms.py
@@ -1,5 +1,6 @@
 from django.forms import ModelForm
 from .models import Project
+from django import forms
 
 
 class ProjectForm(ModelForm):
@@ -7,3 +8,17 @@ class ProjectForm(ModelForm):
         model = Project
         fields = ['title', 'featured_image', 'description', 'demo_link', 'source_link', 'tags']
 
+        widgets = {'tags': forms.CheckboxSelectMultiple()}
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
+
+        # self.fields['title'].widget.attrs.update({'class': 'input'})
+        # self.fields['description'].widget.attrs.update({'class': 'input'})
+        # self.fields['demo_link'].widget.attrs.update({'class': 'input'})
+        # self.fields['source_link'].widget.attrs.update({'class': 'input'})
+        # self.fields['featured_image'].widget.attrs.update({'class': 'input'})
+        # self.fields['tags'].widget.attrs.update({'class': 'input'})
